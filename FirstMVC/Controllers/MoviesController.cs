a0dd01fe0ab7fa47c4e6fb28890117d69ff3285d
liudas <liudas.keturi@outlook.com>
2017-12-17 00:41:42 +0200

    Added model

---
+++ FirstMVC/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+            return View(movie);
+        }
+    }
+}

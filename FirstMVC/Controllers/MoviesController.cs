bc69b320ba1b6823a5790ac304f7d64a9ee00029
liudas <liudas.keturi@outlook.com>
2018-02-17 22:15:07 +0200

    Implemented validation

--- FirstMVC/Controllers/MoviesController.cs
+++ FirstMVC/Controllers/MoviesController.cs
@@ -52,6 +52,15 @@ namespace FirstMVC.Controllers
         [HttpPost]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewMovieViewModel
+                {
+                    Movie = movie,
+                    MovieGenres = _context.MovieGenre.ToList()
+                };
+                return View("New", viewModel);
+            }
             if(movie.Id == 0)
                 _context.Movies.Add(movie);
             else
